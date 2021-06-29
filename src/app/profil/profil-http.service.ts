32cd7c7363ffb4457075a92a6db1eda03ee88b80
Lea <leakidrod@gmail.com>
2021-06-29 15:48:11 +0200

    Manque }

--- src/app/profil/profil-http.service.ts
+++ src/app/profil/profil-http.service.ts
@@ -21,6 +21,7 @@ export class ProfilHttpService {
 
   getIdUtilisateur(): number {
     return JSON.parse(sessionStorage.getItem("utilisateur")).id;
+  }
   loadEquipesSauvegardeesByUtilisateurId(id: number): Observable<Array<Equipe>> {
     return this.http.get<Array<Equipe>>(this.appConfig.backEndUrl +"utilisateur/" + JSON.parse(sessionStorage.getItem("utilisateur")).id + "/equipes")
   }
