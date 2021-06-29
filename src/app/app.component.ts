7af624bd9deb88bb25a96b6ab906a5fd052eea2a
SER <m>
2021-06-29 17:21:19 +0200

    Cacher nav barre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {AccueilHttpService} from "./accueil/accueil-http.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,18 @@ import {AccueilHttpService} from "./accueil/accueil-http.service";
 })
 export class AppComponent {
   title = 'pokebowl-angular';
+  urlEssai: string="";
+  navBarre: Boolean = false;
 
-  constructor(private accueilService: AccueilHttpService) {
+  constructor(private accueilService: AccueilHttpService, private router: Router) {
   }
 
   loadAcceuil() {
     if(JSON.parse(sessionStorage.getItem("utilisateur")).id) {
       this.accueilService.load((JSON.parse(sessionStorage.getItem("utilisateur")).id));
     }
+  }
+  recupererPage(){
+    return this.router.url == '/connexion' || this.router.url == '/inscription';
   }
 }
